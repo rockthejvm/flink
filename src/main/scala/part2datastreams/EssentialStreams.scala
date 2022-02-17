23056c2f2fd4c1c81c89bb573ff0c18583f2b3c6
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2022-02-17 20:47:19 +0200

    explicit transformations

--- src/main/scala/part2datastreams/EssentialStreams.scala
+++ src/main/scala/part2datastreams/EssentialStreams.scala
@@ -1,9 +1,12 @@
 package part2datastreams
 
+import org.apache.flink.api.common.functions.{FlatMapFunction, MapFunction, ReduceFunction}
 import org.apache.flink.api.common.serialization.SimpleStringEncoder
 import org.apache.flink.core.fs.Path
+import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
 
 object EssentialStreams {
 
@@ -95,7 +98,68 @@ object EssentialStreams {
     env.execute()
   }
 
+  // explicit transformations
+  def demoExplicitTransformations(): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val numbers = env.fromSequence(1, 100)
+
+    // map
+    val doubledNumbers = numbers.map(_ * 2)
+
+    // explicit version
+    val doubledNumbers_v2 = numbers.map(new MapFunction[Long, Long] {
+      // declare fields, methods, ...
+      override def map(value: Long) = value * 2
+    })
+
+    // flatMap
+    val expandedNumbers = numbers.flatMap(n => Range.Long(1, n, 1).toList)
+
+    // explicit version
+    val expandedNumbers_v2 = numbers.flatMap(new FlatMapFunction[Long, Long] {
+      // declare fields, methods, ...
+      override def flatMap(n: Long, out: Collector[Long]) =
+        Range.Long(1, n, 1).foreach { i =>
+          out.collect(i) // imperative style - pushes the new element downstream
+        }
+    })
+
+    // process method
+    // ProcessFunction is THE MOST GENERAL function to process elements in Flink
+    val expandedNumbers_v3 = numbers.process(new ProcessFunction[Long, Long] {
+      override def processElement(n: Long, ctx: ProcessFunction[Long, Long]#Context, out: Collector[Long]) =
+        Range.Long(1, n, 1).foreach { i =>
+          out.collect(i)
+        }
+    })
+
+    // reduce
+    // happens on keyed streams
+    /*
+      [ 1, false
+        2, true
+
+        100, true
+
+      true => 2, 6, 12, 20, ...
+      false => 1, 4, 9, 16, ...
+     */
+    val keyedNumbers: KeyedStream[Long, Boolean] = numbers.keyBy(n => n % 2 == 0)
+
+    // reduce - FP approach
+    val sumByKey = keyedNumbers.reduce(_ + _) // sum up all the elements BY KEY
+
+    // reduce - explicit approach
+    val sumByKey_v2 = keyedNumbers.reduce(new ReduceFunction[Long] {
+      // additional fields, methods...
+      override def reduce(x: Long, y: Long): Long = x + y
+    })
+
+    sumByKey_v2.print()
+    env.execute()
+  }
+
   def main(args: Array[String]): Unit = {
-    fizzBuzzExercise()
+    demoExplicitTransformations()
   }
 }
